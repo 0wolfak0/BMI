84dcfe332c8a2c496ae0bc61390bf5cc48aa2f0a
0wolfak0 <148878242+0wolfak0@users.noreply.github.com>
2024-07-28 19:54:11 +0530

    background and message added

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,6 +30,18 @@ class MainActivity : AppCompatActivity() {
                 val bmi = weight / (height * height)
 
                 resultTextView.text = "Your BMI: %.2f".format(bmi)
+                if (bmi < 18.5) {
+                    Toast.makeText(this, "underweight and possibly malnourished", Toast.LENGTH_SHORT).show()
+                }
+                else if (bmi < 24.9) {
+                    Toast.makeText(this, "healthy weight range for young and middle-aged adults", Toast.LENGTH_SHORT).show()
+                }
+                else if (bmi < 29.9) {
+                    Toast.makeText(this, " overweight", Toast.LENGTH_SHORT).show()
+                }
+                else if (bmi > 30) {
+                    Toast.makeText(this, "obese", Toast.LENGTH_SHORT).show()
+                }
             } else {
                 resultTextView.text = "Please enter weight and height."
             }
